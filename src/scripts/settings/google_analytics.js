272d33072640f7051de4b1e51afaa197c6d63fb5
Tiago Porto <tiago@tiagoporto.com>
2016-11-29 21:10:54 -0200

    Turn back GA code

--- src/scripts/settings/google_analytics.js
+++ src/scripts/settings/google_analytics.js
@@ -6,5 +6,6 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-32351360-5', 'auto');
+ga('create', 'UA-32351360-4', 'auto');
+ga('set', 'page', 'swillboilerplate.rocks');
 ga('send', 'pageview');
