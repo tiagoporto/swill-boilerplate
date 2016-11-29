710f88285aa2f81ac4172f20d58affd14948e12d
Tiago Porto <tiago@tiagoporto.com>
2016-11-29 14:27:28 -0200

    Added location on GA

--- src/scripts/settings/google_analytics.js
+++ src/scripts/settings/google_analytics.js
@@ -7,4 +7,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-32351360-4', 'auto');
-ga('send', 'pageview');
+ga('send', 'pageview', location.origin + location.pathname);
