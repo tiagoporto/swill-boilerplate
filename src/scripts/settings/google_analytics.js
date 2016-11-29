f5cdb19e4351773ed70431aa14875366810ace1c
Tiago Porto <tiago@tiagoporto.com>
2016-11-29 21:30:14 -0200

    Fixed GA code

--- src/scripts/settings/google_analytics.js
+++ src/scripts/settings/google_analytics.js
@@ -7,5 +7,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-32351360-4', 'auto');
-ga('set', 'page', 'swillboilerplate.rocks');
-ga('send', 'pageview', location.pathname);
+ga('send', 'pageview', 'swillboilerplate.rocks' + location.pathname);
